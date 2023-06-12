efc8eeafab05cb255a720ae8bd0e13423f2a431b
pengwenhua666 <pengwenhua666@163.com>
2023-06-12 17:53:37 +0800

    feature：创建vue3

--- packages/web/src/main.ts
+++ packages/web/src/main.ts
@@ -3,4 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+// 新增代码：引入全部组件及样式
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+
+
+createApp(App).use(store).use(router).use(Antd).mount('#app')
